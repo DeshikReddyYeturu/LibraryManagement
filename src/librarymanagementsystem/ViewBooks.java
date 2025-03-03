33e9e5eb1dd80b3b5649a447fbc05ba133c5094e
DeshikReddyYeturu <yeturudeshikreddy@gmail.com>
2025-03-03 21:13:26 +0530

    ViewBooks.java

--- src/librarymanagementsystem/ViewBooks.java
+++ src/librarymanagementsystem/ViewBooks.java
@@ -1,12 +1,13 @@
-
 package librarymanagementsystem;
 import javax.swing.*;
 import java.awt.*;
 import java.sql.*;
 import javax.swing.table.DefaultTableModel;
+
 public class ViewBooks extends JFrame {
-    JTable booksTable;
-    DefaultTableModel model;
+
+    private JTable booksTable;
+    private DefaultTableModel model;
 
     ViewBooks() {
         setTitle("View Books");
@@ -22,4 +23,35 @@ public class ViewBooks extends JFrame {
         add(scrollPane, BorderLayout.CENTER);
     }
 
-    
+    @Override
+    public void setVisible(boolean b) {
+        if (b) {
+            refreshData();
+        }
+        super.setVisible(b);
+    }
+    private void refreshData() {
+        model.setRowCount(0);
+
+        try (Connection conn = DataBaseConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT b.book_id, b.book_name, b.author, " +
+                     "(b.quantity - COUNT(CASE WHEN ib.return_date IS NULL THEN ib.book_id ELSE NULL END)) AS available_quantity " +
+                     "FROM books b " +
+                     "LEFT JOIN issued_books ib ON b.book_id = ib.book_id " +
+                     "GROUP BY b.book_id, b.book_name, b.author")) {
+            while (rs.next()) {
+                String bookId = rs.getString("book_id");
+                String bookName = rs.getString("book_name");
+                String author = rs.getString("author");
+                int availableQuantity = rs.getInt("available_quantity");
+
+                model.addRow(new Object[]{bookId, bookName, author, availableQuantity});
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(this, "Database error: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
